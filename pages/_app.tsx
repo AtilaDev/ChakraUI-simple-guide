957663ea2acb1f3dc9b3736339b46b47acc6cff6
Leandro <atiladevelop@gmail.com>
2022-01-01 18:57:45 -0300

    Chagra project guide done.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,15 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import { AppProps } from 'next/app';
+import { ChakraProvider } from '@chakra-ui/react';
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
-}
+import theme from '../src/theme';
+import '../src/theme/styles.css';
 
-export default MyApp
+const App = ({ Component, pageProps }: AppProps) => {
+  return (
+    <ChakraProvider theme={theme}>
+      <Component {...pageProps} />
+    </ChakraProvider>
+  );
+};
+
+export default App;
